286df69106447ec6bce0cdf3a598f3aa0026385a
ILYASS ELMAMOUNI <ilyasselmamouni0@gmail.com>
2025-06-22 12:56:36 +0300

    save

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -54,7 +54,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-          Learn More!
+          Learn More!.
         </motion.a>
       </div>
 
